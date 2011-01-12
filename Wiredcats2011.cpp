a148a0a1305b070a92e4467fa50751cb1e8300f6
The Westminster Wiredcats (FRC 2415) <wiredcats2415@gmail.com>
2011-01-11 21:57:31 -0500

    2415d up the default code

---
+++ Wiredcats2011.cpp
@@ -0,0 +1,16 @@
+#include "Wiredcats2011.h"
+
+Wiredcats2011::Wiredcats2011(void) {
+	
+}
+
+Wiredcats2011::Autonomous(void) {
+	
+}
+
+Wiredcats2011::OperatorControl(void) {
+	
+}
+
+START_ROBOT_CLASS(Wiredcats2011);
+
---
+++ Wiredcats2011.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "WPILib.h"
+
+class Wiredcats2011 : public SimpleRobot {
+public:
+	Wiredcats2011();
+	void Autonomous();
+	void OperatorControl();
+};
